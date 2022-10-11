68f5b630fb12175382ade42e1c486f52e3b11178
Francis Chan <franciszero@gmail.com>
2022-10-11 18:33:33 -0400

    .

--- app.py
+++ app.py
@@ -57,47 +57,47 @@ def index():
     country = str(r["sys"]["country"])
     # build the Dialogflow reply.
     reply = '{"fulfillmentMessages": [ {"text": {"text": ["Currently in ' + city + ', ' + country + ' it is ' + temp + ' degrees and ' + weather + '"] } } ]}'
-    reply = '''{"fulfillmentMessages": 
-[{
-  "telegram": {
-    "text": "Pick a color",
-    "reply_markup": {
-      "inline_keyboard": [
-        [
-          {
-            "text": "Red",
-            "callback_data": "Red"
-          }
-        ],
-        [
-          {
-            "text": "Green",
-            "callback_data": "Green"
-          }
-        ],
-        [
-          {
-            "text": "Yellow",
-            "callback_data": "Yellow"
-          }
-        ],
-        [
-          {
-            "text": "Blue",
-            "callback_data": "Blue"
-          }
-        ],
-        [
-          {
-            "text": "Pink",
-            "callback_data": "Pink"
-          }
-        ]
-      ]
-    }
-  }
-}]
-}'''
+#     reply = '''{"fulfillmentMessages":
+# [{
+#   "telegram": {
+#     "text": "Pick a color",
+#     "reply_markup": {
+#       "inline_keyboard": [
+#         [
+#           {
+#             "text": "Red",
+#             "callback_data": "Red"
+#           }
+#         ],
+#         [
+#           {
+#             "text": "Green",
+#             "callback_data": "Green"
+#           }
+#         ],
+#         [
+#           {
+#             "text": "Yellow",
+#             "callback_data": "Yellow"
+#           }
+#         ],
+#         [
+#           {
+#             "text": "Blue",
+#             "callback_data": "Blue"
+#           }
+#         ],
+#         [
+#           {
+#             "text": "Pink",
+#             "callback_data": "Pink"
+#           }
+#         ]
+#       ]
+#     }
+#   }
+# }]
+# }'''
     return reply
 
 
