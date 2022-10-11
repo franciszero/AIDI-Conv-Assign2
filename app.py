9f4e9fc411988a5b8a2da8c2e9f2e0a25dfbef44
Francis Chan <franciszero@gmail.com>
2022-10-11 17:30:47 -0400

    test

--- app.py
+++ app.py
@@ -57,44 +57,10 @@ def index():
     country = str(r["sys"]["country"])
     # build the Dialogflow reply.
     reply = '{"fulfillmentMessages": [ {"text": {"text": ["Currently in ' + city + ', ' + country + ' it is ' + temp + ' degrees and ' + weather + '"] } } ]}'
-    reply = '''
-    {
+    reply = '''{
   "telegram": {
-    "text": "Pick a color",
-    "reply_markup": {
-      "inline_keyboard": [
-        [
-          {
-            "text": "Red",
-            "callback_data": "Red"
-          }
-        ],
-        [
-          {
-            "text": "Green",
-            "callback_data": "Green"
-          }
-        ],
-        [
-          {
-            "text": "Yellow",
-            "callback_data": "Yellow"
-          }
-        ],
-        [
-          {
-            "text": "Blue",
-            "callback_data": "Blue"
-          }
-        ],
-        [
-          {
-            "text": "Pink",
-            "callback_data": "Pink"
-          }
-        ]
-      ]
-    }
+    "text": "You can read about *entities* [here](/docs/concept-entities).",
+    "parse_mode": "Markdown"
   }
 }'''
     return reply
